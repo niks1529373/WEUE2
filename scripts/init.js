e3817f91fb6243a81ccae5d76c4d696575ba8067
Niko Schrack <e01529373@tuwien.ac.at>
2018-04-16 18:26:00 +0200

    added basic init.js functinality
    not finished

--- scripts/init.js
+++ scripts/init.js
@@ -1,10 +1,22 @@
 $(document).ready(function() {
 
     // TODO init: initialize all counters
+    var deviceCounter = new Counter($('.devices-counter'));
+    var arrowsCounter = new Counter($('.arrows-counter'));
 
     // TODO init: initialize controls
+    var controls = new Controls($('#controls'));
 
     // TODO init: initialize diagram and transfer counters and controls
+    new Diagram('#diagram', '#arrow-sidebar-add', deviceCounter, arrowsCounter, controls);
 
     // TODO init: add drag functionality to devices in sidebar
+    $('.device').draggable({
+    	helper:  function() {
+        	return $(this).children('.device-image').clone();
+    	}
+	});
+
+    $('#diagram').droppable();
+
 });
